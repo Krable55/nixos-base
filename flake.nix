285f798683ac1408c2fffea8a764e0f79a654504
Kyle Rable <Krable55@gmail.com>
2025-04-13 09:00:01 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,29 @@
 {
-  description = "Base NixOS system config";
+  description = "Base NixOS image and system config";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, nixos-generators, ... }: {
+    packages = {
+      x86_64-linux = {
+        default = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+          format = "proxmox";
+        };
+      };
+    };
+
     nixosConfigurations = {
-      nixos-builder = nixpkgs.lib.nixosSystem {
+      base = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-        ];
+        modules = [ ./configuration.nix ];
       };
     };
   };
