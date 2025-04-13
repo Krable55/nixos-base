47111549297b34a28f12a6c1b37b4a18af191ecb
Kyle Rable <Krable55@gmail.com>
2025-04-13 08:20:07 -0700

    Update flake.nix
    Fix flake

--- flake.nix
+++ flake.nix
@@ -1,87 +1,20 @@
-{ config, pkgs, modulesPath, lib, system, ... }:
-
 {
-  imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
-  ];
-
-  config = {
-    #Provide a default hostname
-    networking.hostName = lib.mkDefault "base";
-    networking.useDHCP = lib.mkDefault true;
-    
-    # Enable QEMU Guest for Proxmox
-    services.qemuGuest.enable = lib.mkDefault true;
-
-    # Use the boot drive for grub
-    boot.loader.grub.enable = lib.mkDefault true;
-    boot.loader.grub.devices = [ "nodev" ];
-
-    boot.growPartition = lib.mkDefault true;
-
-    # Allow remote updates with flakes and non-root users
-    nix.settings.trusted-users = [ "root" "@wheel" ];
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-    networking.networkmanager.enable = true;
-  
-    # Enable Tailscale
-    services.tailscale.enable = true;
-    services.tailscale.useRoutingFeatures = "client";
-    services.tailscale.authKeyFile = "/var/lib/tailscale/authkey";
-
-    # Set your time zone.
-    time.timeZone = "America/Los_Angeles";
-
-    # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
-    
-    # Enable mDNS for `hostname.local` addresses
-    services.avahi.enable = true;
-    services.avahi.nssmdns4 = true;
-    services.avahi.publish = {
-      enable = true;
-      addresses = true;
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # Some sane packages we need on every system
-    environment.systemPackages = with pkgs; [
-      vim  # for emergencies
-      git # for pulling nix flakes
-    ];
-
-    # Don't ask for passwords
-    security.sudo.wheelNeedsPassword = false;
-
-    # Enable ssh
-    services.openssh = {
-      enable = true;
-      settings.PasswordAuthentication = false;
-      settings.KbdInteractiveAuthentication = false;
+  };
+  outputs = { self, nixpkgs, nixos-generators, ... }: {
+    packages.x86_64-linux = {
+      default = nixos-generators.nixosGenerate {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+        format = "proxmox";
+      };
     };
-    programs.ssh.startAgent = true;
-
-    # Default user
-    users.users.kyle = {
-     isNormalUser = true;
-     description = "Kyle Rable";
-     extraGroups = [ "networkmanager" "wheel" "docker"];
-     packages = with pkgs; [
-      vim
-      git
-     ];
-     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMh4QcZWNLqRMaj0D5iKH3FDZ8n/rKJR6XFLNQs7bWsa my key for nixos"
-     ];
-    };
-    
-    # Default filesystem
-    fileSystems."/" = lib.mkDefault {
-      device = "/dev/disk/by-label/nixos";
-      autoResize = true;
-      fsType = "ext4";
-    };
-
-    system.stateVersion = lib.mkDefault "24.11";
   };
 }
