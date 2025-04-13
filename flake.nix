eafa55abfe6194aace33f40111f01afc930279b4
Kyle Rable <Krable55@gmail.com>
2025-04-13 08:25:52 -0700

    Update flake.nix
    Add support for system configuration

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Base NixOS image and system config";
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixos-generators = {
@@ -6,15 +8,21 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
   outputs = { self, nixpkgs, nixos-generators, ... }: {
-    packages.x86_64-linux = {
-      default = nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-        ];
-        format = "proxmox";
-      };
+    packages.x86_64-linux.default = nixos-generators.nixosGenerate {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+      format = "proxmox";
+    };
+
+    nixosConfigurations.base = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
     };
   };
 }
