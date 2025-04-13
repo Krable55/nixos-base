72e079235b860eabc5ee70fe84827fb4d629e7e8
Kyle Rable <Krable55@gmail.com>
2025-04-13 09:14:13 -0700

    Add builder output

--- flake.nix
+++ flake.nix
@@ -1,18 +1,26 @@
 {
-  description = "Base NixOS image and system config";
+  description = "NixOS system flake with image building and system config support";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations = {
-      base = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-        ];
-      };
+  outputs = { self, nixpkgs, nixos-generators, ... }: {
+    # VM image builder (e.g., for Proxmox)
+    packages.x86_64-linux.default = nixos-generators.nixosGenerate {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+      format = "proxmox";
+    };
+
+    # NixOS config for installation or configuration management
+    nixosConfigurations."nixos-builder" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
     };
   };
 }
