a8aa7c017e74140a0a8f9678ed107d70bb05644f
Kyle Rable <Krable55@gmail.com>
2025-04-14 23:32:02 -0700

    Get rid of merege

--- modules/media.nix
+++ modules/media.nix
@@ -7,8 +7,7 @@ in {
     enable = lib.mkEnableOption "Enable media apps and services";
   };
 
-  config = lib.mkIf cfg.enable (lib.mkMerge [
-  {
+  config = lib.mkIf cfg.enable {
     users.groups.media.members = [
       "kyle"
       "sonarr"
@@ -23,39 +22,41 @@ in {
       "d /mnt/media 0775 media media -"
     ];
 
-    services = {
-      nfs.client.enable = true;
+    services.nfs.client.enable = true;
 
-      sonarr = {
-        enable = true;
-        openFirewall = true;
-        group = "media";
-      };
-      radarr = {
-        enable = true;
-        openFirewall = true;
-        group = "media";
-      };
-      lidarr = {
-        enable = true;
-        openFirewall = true;
-        group = "media";
-      };
-      readarr = {
-        enable = true;
-        openFirewall = true;
-        group = "media";
-      };
-      prowlarr = {
-        enable = true;
-        openFirewall = true;
-        # group = "media";
-      };
-      tautulli = {
-        enable = true;
-        openFirewall = true;
-        group = "media";
-      };
+    services.sonarr = {
+      enable = true;
+      openFirewall = true;
+      group = "media";
+    };
+
+    services.radarr = {
+      enable = true;
+      openFirewall = true;
+      group = "media";
+    };
+
+    services.lidarr = {
+      enable = true;
+      openFirewall = true;
+      group = "media";
+    };
+
+    services.readarr = {
+      enable = true;
+      openFirewall = true;
+      group = "media";
+    };
+
+    services.prowlarr = {
+      enable = true;
+      openFirewall = true;
+    };
+
+    services.tautulli = {
+      enable = true;
+      openFirewall = true;
+      group = "media";
     };
 
     fileSystems."/mnt/media" = lib.mkForce {
@@ -64,26 +65,25 @@ in {
       options = [ "x-systemd.automount" "noauto" "_netdev" ];
     };
 
-    virtualisation = {
-      docker.enable = true;
-      oci-containers.backend = "docker";
-      oci-containers.containers.overseerr = {
-        image = "lscr.io/linuxserver/overseerr:latest";
-        ports = [ "5055:5055" ];
-        environment = {
-          PGID = "1000";
-          PUID = "1000";
-          TZ = "America/Los_Angeles";
-        };
-        volumes = [
-          "/mnt/media/apps/overseer:/config:rw"
-        ];
+    virtualisation.docker.enable = true;
+
+    virtualisation.oci-containers.backend = "docker";
+
+    virtualisation.oci-containers.containers.overseerr = {
+      image = "lscr.io/linuxserver/overseerr:latest";
+      ports = [ "5055:5055" ];
+      environment = {
+        PGID = "1000";
+        PUID = "1000";
+        TZ = "America/Los_Angeles";
       };
+      volumes = [
+        "/mnt/media/apps/overseer:/config:rw"
+      ];
     };
 
     networking.firewall.allowedTCPPorts = [
       5055 8989 7878 8686 8787 8181 9696
     ];
-  }
-]);
+  };
 }
