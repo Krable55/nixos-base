c55b7d3b55a7d1aa478d32db34c1a466ae1325fe
Kyle Rable <Krable55@gmail.com>
2025-04-14 21:32:13 -0700

    Add services.nfs.client.enable = true;

--- modules/media.nix
+++ modules/media.nix
@@ -27,6 +27,7 @@ in {
       ];
 
       # Define the NFS mount
+      services.nfs.client.enable = true;
       fileSystems."/mnt/media" = {
         device = "192.168.50.154:/MediaCenter";
         fsType = "nfs";
