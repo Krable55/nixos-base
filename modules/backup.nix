d7b4cd7207872aee322fe702785222ec68441f8d
Kyle Rable <Krable55@gmail.com>
2025-04-16 23:04:02 -0700

    fix

--- modules/backup.nix
+++ modules/backup.nix
@@ -16,24 +16,36 @@ let
     WEEKLY=${toString cfg.retention.weekly}
     MONTHLY=${toString cfg.retention.monthly}
 
+    MANUALP="manual"
     DAILYP="daily"
     WEEKLYP="weekly"
     MONTHLYP="monthly"
     LOGSP="logs"
-    LAST="last"
 
-    for d in "$DAILYP" "$WEEKLYP" "$MONTHLYP" "$LOGSP"; do
+    for d in "$MANUALP" "$DAILYP" "$WEEKLYP" "$MONTHLYP" "$LOGSP"; do
       mkdir -p "$BCKP/$d"
     done
 
-    TODAY=$(find "$BCKP/$DAILYP" -maxdepth 1 -mindepth 1 -type d -ctime -1 -printf "a" | wc -c)
-    if [ "$TODAY" -gt 0 ]; then
-      echo "Backup already performed in last 24h. Exiting."
-      exit 0
+    if [ $# -ge 1 ]; then
+      FOLDER="$MANUALP"
+      NAME="$1"
+    else
+      FOLDER="$DAILYP"
+      TODAY=$(find "$BCKP/$FOLDER" -maxdepth 1 -mindepth 1 -type d -ctime -1 -printf "a" | wc -c)
+      if [ $TODAY -gt 0 ]; then
+        echo "Backup already performed in last 24h. Exiting."
+        exit 0
+      fi
+      NAME=$(date +%Y-%m-%d_%H-%M)
     fi
 
-    NAME=$(date +%Y-%m-%d_%H-%M)
     LOG="$BCKP/$LOGSP/$NAME.log"
+
+    if [ $# -ge 2 ]; then
+      LAST="$2"
+    else
+      LAST="last"
+    fi
 
     if [ -e "$BCKP/$LAST" ]; then
       LINK="--link-dest=$BCKP/$LAST/"
@@ -42,18 +54,18 @@ let
     fi
 
     echo "Running rsync..." >> "$LOG"
-    rsync -aAXiH ${includes} --include='*/' --exclude='*' $LINK "$SRC/" "$BCKP/$DAILYP/$NAME/" >> "$LOG" 2>&1 || echo "rsync failed" >> "$LOG"
+    rsync -aAXiH ${includes} --include='*/' --exclude='*' $LINK "$SRC/" "$BCKP/$FOLDER/$NAME/" >> "$LOG" 2>&1 || echo "rsync failed" >> "$LOG"
 
     if [ -L "$BCKP/$LAST" ]; then
       echo "Removing old symlink $BCKP/$LAST" >> "$LOG"
       rm -f "$BCKP/$LAST"
     fi
 
-    ln -s "$BCKP/$DAILYP/$NAME" "$BCKP/$LAST" 2>>"$LOG"
+    ln -s "$BCKP/$FOLDER/$NAME" "$BCKP/$LAST" 2>>"$LOG"
 
     remove() {
       COUNT=$(find "$1" -maxdepth 1 -mindepth 1 -type d -printf "a" | wc -c)
-      while [ "$COUNT" -gt "$2" ]; do
+      while [ $COUNT -gt $2 ]; do
         OLDEST=$(find "$1" -maxdepth 1 -mindepth 1 -type d -printf "%C+ %p\0" | sort -z | grep -zom 1 ".*" | sed 's/[^ ]* //')
         echo "Removing $OLDEST" >> "$LOG"
         rm -r --interactive=never "$OLDEST" >> "$LOG"
@@ -67,24 +79,23 @@ let
       while IFS=  read -r -d $'\0'; do
         NEWFILES+=("$REPLY")
         ((COUNT+=1))
-      done < <(find "$1" -maxdepth 1 -mindepth 1 -type d -ctime -"$3" -printf "%p\0")
+      done < <(find "$1" -maxdepth 1 -mindepth 1 -type d -ctime -$3 -printf "%p\0")
 
-      if [ "$COUNT" -eq 0 ]; then
+      if [ $COUNT -eq 0 ]; then
         OLDEST=$(find "$2" -maxdepth 1 -mindepth 1 -type d -printf "%C+ %p\0" | sort -z | grep -zom 1 ".*" | sed 's/[^ ]* //')
-        if [ -n "$OLDEST" ]; then
+        if [ ! -z "$OLDEST" ]; then
           echo "Moving $OLDEST from $2 to $1" >> "$LOG"
           mv "$OLDEST" "$1" >> "$LOG"
         fi
       fi
 
-      remove "$1" "$4"
+      remove "$1" $4
     }
 
-    move_and_remove "$BCKP/$MONTHLYP" "$BCKP/$WEEKLYP" "$MONTH" "$MONTHLY"
-    move_and_remove "$BCKP/$WEEKLYP" "$BCKP/$DAILYP" "$WEEK" "$WEEKLY"
-    remove "$BCKP/$DAILYP" "$DAILY"
+    move_and_remove "$BCKP/$MONTHLYP" "$BCKP/$WEEKLYP" $MONTH $MONTHLY
+    move_and_remove "$BCKP/$WEEKLYP" "$BCKP/$DAILYP" $WEEK $WEEKLY
+    remove "$BCKP/$DAILYP" $DAILY
   '';
-
   scriptFile = pkgs.writeShellScript "rsync-backup" backupScript;
 in {
   options.custom.backup = {
