f1e508d7da580f29118fcf367db63e109a072614
Kyle Rable <Krable55@gmail.com>
2025-04-16 22:27:23 -0700

    Update forgejo module

--- modules/forgejo.nix
+++ modules/forgejo.nix
@@ -1,45 +1,53 @@
 { config, pkgs, lib, ... }:
 
-{
-  services.forgejo = {
-    enable = true;
+let
+  cfg = config.custom.forgejo;
+in {
+  options.custom.forgejo = {
+    enable = lib.mkEnableOption "Enable Forgejo server and runner configuration";
+  };
 
-    user = "forgejo";
-    group = "forgejo";
+  config = lib.mkIf cfg.enable {
+    services.forgejo = {
+      enable = true;
 
-    settings = {
-      server = {
-        DOMAIN = "git.local";
-        HTTP_PORT = 3000;
-        ROOT_URL = "http://git.local:3000/";
-      };
+      user = "forgejo";
+      group = "forgejo";
 
-      database = {
-        DB_TYPE = "sqlite3";
-        PATH = "/var/lib/forgejo/data/forgejo.db";
-      };
+      settings = {
+        server = {
+          DOMAIN = "git.local";
+          HTTP_PORT = 3000;
+          ROOT_URL = "http://git.local:3000/";
+        };
 
-      repository = {
-        ROOT = lib.mkForce "/var/lib/forgejo/repos";
+        database = {
+          DB_TYPE = "sqlite3";
+          PATH = "/var/lib/forgejo/data/forgejo.db";
+        };
+
+        repository = {
+          ROOT = lib.mkForce "/var/lib/forgejo/repos";
+        };
       };
     };
+
+    users.users.forgejo = {
+      isSystemUser = true;
+      home = "/var/lib/forgejo";
+      createHome = true;
+      group = "forgejo";
+    };
+
+    users.groups.forgejo = {};
+
+    users.users.forgejo-runner = {
+      isSystemUser = true;
+      group = "forgejo";
+      home = "/var/lib/forgejo-runner";
+      createHome = true;
+    };
+
+    networking.firewall.allowedTCPPorts = [ 3000 ];
   };
-
-  users.users.forgejo = {
-    isSystemUser = true;
-    home = "/var/lib/forgejo";
-    createHome = true;
-    group = "forgejo";
-  };
-
-  users.groups.forgejo = { };
-
-  users.users.forgejo-runner = {
-    isSystemUser = true;
-    group = "forgejo"; # or "nogroup"
-    home = "/var/lib/forgejo-runner";
-    createHome = true;
-  };
-
-  networking.firewall.allowedTCPPorts = [ 3000 ];
 }
